a15ed830e3858dc0bcb1c20c20f052c5c5e6552b
Anand Pawara <anandpawara19@gmail.com>
2018-04-07 12:14:44 +0530

    first

---
+++ C/LinkedList_inserting_node.c
@@ -0,0 +1,13 @@
+//This code is taken from https://www.geeksforgeeks.org/linked-list-set-2-inserting-a-node/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+	int data;
+	Struct Node *next'
+};
+
+
+
